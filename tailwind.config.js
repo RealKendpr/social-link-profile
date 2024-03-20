ace94606cd3367f61b0cc4e9eb9132f966199458
kendpr <real.kendpr@gmail.com>
2024-03-20 20:21:50 +0800

    CSS (Tailwind)
    - Added the appropriate colors
    - Hover effects
    - Responsiveness to large screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "hsl(75, 94%, 57%)",
+        neutral: {
+          50: "hsl(0, 0%, 100%)",
+          500: "hsl(0, 0%, 20%)",
+          900: "hsl(0, 0%, 12%)",
+          950: "hsl(0, 0%, 8%)",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
